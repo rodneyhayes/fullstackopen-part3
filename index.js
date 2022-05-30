03b4a89466be519430dda3f104a3f54c72743c0b
rodneyhayes <rodneyhayes@icloud.com>
2022-05-30 14:14:27 -0700

    Part 3 exercise 3.2

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ app.get('/api/persons', (req, res) => {
     res.json(persons);
 });
 
+app.get('/info', (req, res) => {
+    res.send(`<p>Phonebook has info for ${persons.length} ${persons.length === 1 ? 'person' : 'people'}</p><p>${new Date()}</p>`)
+});
+
 const PORT = 3001;
 
 app.listen(PORT, () => {
